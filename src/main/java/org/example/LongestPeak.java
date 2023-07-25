dfcbc4af5e8875a69abfc750d3b04255b27ae1d7
ShikharSingh2205 <91791888+ShikharSingh2205@users.noreply.github.com>
2023-07-25 12:24:12 +0530

    Update LongestPeak.java

--- src/main/java/org/example/LongestPeak.java
+++ src/main/java/org/example/LongestPeak.java
@@ -6,6 +6,9 @@ public class LongestPeak {
         int[] array = new int[arr.length];
         for (int i = 0; i < arr.length; i+=2) {
             array[i]=Integer.parseInt(arr[i]);
+            //changes done 
+            array[i]=array[i]+i;
+            
         }
         return -1;
     }
